7babc1960ec2e83d0ee17a5796992df24595f0e2
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-03-26 19:25:29 +0000

    Added temperature readings.

--- app/src/arduino/get_data/get_data.ino
+++ app/src/arduino/get_data/get_data.ino
@@ -1,9 +1,41 @@
+#include <Arduino.h>
+#include "LM35IC.h"
+
+uint8_t temperature_pin = A0;
+uint8_t humidity_pin = A1;
+uint8_t light_pin = A2;
+
+double Ts = 1.0 * 1000.0;
+double time = 0.0;
+
+using LM35::LM35IC;
+
+LM35IC temperature_sensor = LM35IC(temperature_pin);
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  //Serial.print("Humidity (%)");
+  //Serial.println("Light (lux)");
+
+  pinMode(temperature_pin, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  double current_time = millis();
+  double temperature = 0.0;
+
+  if(current_time - time >= Ts){
+
+    temperature = temperature_sensor.readTemp();
+    //temperature = analogRead(temperature_pin);
+    Serial.print("Temperature:");
+    Serial.println(temperature);
+
+    time = current_time;
+
+  }
 
 }
