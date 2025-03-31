6daa9c1300b3909f2e6cd830da86feeb1632b180
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-03-31 17:25:34 +0100

    Finished code for light sensor readings (sensor parameters are wrong).

--- app/src/arduino/get_data/get_data.ino
+++ app/src/arduino/get_data/get_data.ino
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+#include <stdint.h>
+#include <math.h>
 #include "DHT11.h"
+
+double adc_res = 5.0 / 1024.0;
 
 uint8_t humidity_pin = 4;
 uint8_t light_pin = A2;
@@ -21,20 +25,33 @@ void setup() {
 void loop() {
   
   double current_time = millis();
+  int temperature = 0;
+  int humidity = 0;
+  int result = 0;
+  uint8_t adc_reading = 0;
+  double Vout = 0;
+  double RL = 0;
+  double lux = 0;
 
   if(current_time - time >= Ts){
 
-    int temperature = 0;
-    int humidity = 0;
-
     // Attempt to read the temperature and humidity values from the DHT11 sensor.
-    int result = sensor.readTemperatureHumidity(temperature, humidity);
+    result = sensor.readTemperatureHumidity(temperature, humidity);
+
+    adc_reading = analogRead(light_pin);
+    Vout = static_cast<double>(adc_reading) * adc_res;
+    RL = 10.0 * (5.0 / Vout - 1.0);
+    lux = pow(10.0, 1.6 / 0.6 - 1.0 / 0.6 * log10(RL));
+
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.print(" lux");
 
     // Check the results of the readings.
     // If the reading is successful, print the temperature and humidity values.
     // If there are errors, print the appropriate error messages.
     if (result == 0) {
-        Serial.print("Temperature: ");
+        Serial.print("\tTemperature: ");
         Serial.print(temperature);
         Serial.print(" °C\tHumidity: ");
         Serial.print(humidity);
